3a4dab048a44095635086e9b588cb918edeabfd9
stanislavasal <stanislavasalishcheva@yandex.ru>
2023-10-10 19:05:03 +0300

    The end???

--- javascripts/scr.js
+++ javascripts/scr.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(".img4").click(function () {
     $(".content2").css("display", "none");
-    $(".content1").css("display", "block");
+    $(".content1").css("display", "flex");
   });
 });
